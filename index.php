b87a32edbae07c473d87d517cc5659a98ef22331
Liew Hui Lek <huilekliew@gmail.com>
2021-11-23 16:15:35 +0800

    Adjust the content of header and body in index.php

--- index.php
+++ index.php
@@ -16,16 +16,16 @@
 </head>
 
 <body>
-    <div class="header">
+    <div class="header container">
         <div class="container">
             <h1>PDF to TXT Converter</h1>
-            <h2>Convert your pdf files to txt files</h2>
         </div>
-
     </div>
+
     <div class="main">
         <div class="container">
-            <h3>Please upload your pdf file</h3>
+            <h2>Convert PDF files to Text files</h2>
+            <h3>Please upload your PDF file</h3>
             <form action="upload.php" method="post", enctype="multipart/form-data">
                 <input type="file" id="myFile" class="file-upload" name="uploaded_file", value="uploaded_file">
                 <div> <?php echo $message; ?> </div>
@@ -33,9 +33,10 @@
                 <button type="submit" class="submit">Submit</button>
             </form>
         </div>
-        <p>continue to put whatever you want</p>
+        <p>Continue to put whatever you want</p>
     </div>
 </body>
+
 <footer class="footer">
     <p>Credits:</p>
     <ul>
@@ -43,9 +44,8 @@
         <li>Liew Hui Lek 151496</li>
         <li>Teh Zhen Jun 153566</li>
         <li>Shaun Liew Xin Hong 154746</li>
-
     </ul>
-    <p>&copy; CAT201 assignment 1 2021-2022</p>
+    <p>&copy; CAT201 Assignment 1 2021-2022</p>
 </footer>
 
 </html>
