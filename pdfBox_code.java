279b115cbee7317529af3b2cb559e49514f03e5f
tehteh22 <79691592+tehteh22@users.noreply.github.com>
2021-12-11 15:27:42 +0800

    Add files via upload

---
+++ pdfBox_code.java
@@ -0,0 +1,30 @@
+package pdfBox.src.main;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.PrintWriter;
+
+public class pdfBox_code {
+    public static void main(String[] args) throws IOException {
+       
+        File file = new File("assignment1-main\\pdfBox\\src\\upload\\sample.pdf");
+        String text;
+        PDFParser parser = new PDFParser(new RandomAccessFile(file, "r"));
+        parser.parse();
+
+        COSDocument cosDoc = parser.getDocument();
+        PDFTextStripper pdfStrip = new PDFTextStripper();
+        PDDocument pdDoc = new PDDocument(cosDoc);
+        text = pdfStrip.getText(pdDoc);
+
+        PrintWriter priWri = new PrintWriter("assignment1-main\\pdfBox\\src\\upload\\sample.pdf");
+        priWri.print(text);
+        priWri.close();
+    }
+}
+
